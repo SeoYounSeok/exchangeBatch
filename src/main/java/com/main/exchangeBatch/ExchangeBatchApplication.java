8032d183295ac0b13fc04ff33941956da613ac39
SeoYounSeok <syounseok94@gmail.com>
2023-11-22 22:44:30 +0900

    오류 확인을 위한 스케줄러 삭제

--- src/main/java/com/main/exchangeBatch/ExchangeBatchApplication.java
+++ src/main/java/com/main/exchangeBatch/ExchangeBatchApplication.java
@@ -2,13 +2,12 @@ package com.main.exchangeBatch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
+
 @SpringBootApplication
 public class ExchangeBatchApplication {
 
-
 	public static void main(String[] args) {
-		// 스프링 애플리케이션을 실행하고 컨텍스트를 얻어옵니다.
-		ConfigurableApplicationContext context = SpringApplication.run(ExchangeBatchApplication.class, args);
+		// 스프링 애플리케이션을 실행합니다.
+		SpringApplication.run(ExchangeBatchApplication.class, args);
 	}
 }
